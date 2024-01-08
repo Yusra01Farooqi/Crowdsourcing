c3a427231cd2e17fbe4289e4f87b20a70fa318e6
Florian <florian.schlechte@tu-ilmenau.de>
2024-01-09 00:15:02 +0100

    _

--- colab_utils/clear_results.py
+++ colab_utils/clear_results.py
@@ -22,7 +22,7 @@ def empty_directories(directory_path, target_directories):
                     for filename in os.listdir(target_directory_path):
                         file_path = os.path.join(target_directory_path, filename)
                         try:
-                            if os.path.isfile(file_path):
+                            if os.path.isfile(file_path): 
                                 os.unlink(file_path)
                             elif os.path.isdir(file_path):
                                 shutil.rmtree(file_path)
